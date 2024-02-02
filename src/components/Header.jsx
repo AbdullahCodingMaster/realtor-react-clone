edcd03be3cb515bcfe6d9360fcacc7d1da6ef0aa
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-02-02 20:46:40 +0500

    Adding padding in logo div container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ const Header = () => {
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
       <header className="flex justify-between align-items-center  px-3 max-w-6xl mx-auto">
-        <div className="">
+        <div className="py-3">
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
             alt="realtor logo"
-            className="h-5 cursor-pointer  "
+            className="h-5 cursor-pointer"
             onClick={() => navigate("/")}
           />
         </div>
