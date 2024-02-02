fa6df55521d5dd70ccb01357142b96892c5ba92c
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-02-02 20:36:25 +0500

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,53 @@
+import { useLocation, useNavigate } from "react-router";
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) return true;
+  };
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between align-items-center  px-3 max-w-6xl mx-auto">
+        <div className="">
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="realtor logo"
+            className="h-5 cursor-pointer  "
+            onClick={() => navigate("/")}
+          />
+        </div>
+
+        <div>
+          <ul className="flex space-x-10 ">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/sign-in")}
+            >
+              Sign In
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
