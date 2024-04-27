ec52d25a77b67e4a20dd771423a615181851d215
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-04-27 14:49:41 +0500

    create private route and custom hook for protecting profile page and complete logout functionality successfuly.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import { getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+
+  const { name, email } = formData;
+
+  const onchange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const handleSignOutClick = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <section className="flex justify-center items-center flex-col max-w-6xl mx-auto">
+      <h1 className="text-3xl text-center mt-6 font-bold">Profile</h1>
+      <div className="w-full md:w-[50%] mt-6 px-3">
+        <form>
+          <input
+            disabled
+            type="name"
+            id="name"
+            value={name}
+            onChange={onchange}
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+          />
+          <input
+            disabled
+            type="email"
+            id="email"
+            value={email}
+            onChange={onchange}
+            className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+          />
+
+          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+            <p className="flex items-center">
+              Don't you want to change your name?{" "}
+              <span className="text-red-600 hover:text-red-800 transition duration-200 ml-1 cursor-pointer ease-in-out ">
+                Edit
+              </span>
+            </p>
+            <p
+              onClick={handleSignOutClick}
+              className="text-blue-600 hover:text-blue-800 transition duration-200 cursor-pointer ease-in-out "
+            >
+              Sign out
+            </p>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Profile;
