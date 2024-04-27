362843b89e4b28163aac183de3c2f6ef95521901
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-04-27 19:12:24 +0500

    complete the header component by adding dynamic content on the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
 const Header = () => {
+  const [pageState, setPageState] = useState("Sign in");
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else setPageState("Sign in");
+    });
+  }, [auth]);
 
   const pathMatchRoute = (route) => {
     if (route === location.pathname) return true;
@@ -40,13 +52,12 @@ const Header = () => {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-transparent ${
-                pathMatchRoute("/sign-in")
-                  ? "text-gray-950 border-b-red-500"
-                  : ""
+                pathMatchRoute("/sign-in") ||
+                (pathMatchRoute("/profile") && "text-gray-950 border-b-red-500")
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
