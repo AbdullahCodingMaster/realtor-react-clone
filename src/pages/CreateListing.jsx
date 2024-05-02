3ee7997df98fb6ec9a0046c5f06a98a876c1e3d0
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-05-02 22:17:33 +0500

    add functionality to the create listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,28 @@
 import React, { useState } from "react";
-
+import Spinner from "./../components/Spinner";
+import { toast } from "react-toastify";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { v4 as uuidv4 } from "uuid";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "./../firebase";
+import { useNavigate } from "react-router-dom";
+const apiKey = process.env.REACT_APP_GEOCODE_API_KEY;
 export default function CreateListing() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -13,6 +35,9 @@ export default function CreateListing() {
     offer: false,
     regularPrice: 0,
     discountedPrice: 0,
+    latitude: 0,
+    longitude: 0,
+    images: {},
   });
 
   const {
@@ -27,19 +52,158 @@ export default function CreateListing() {
     offer,
     regularPrice,
     discountedPrice,
+    latitude,
+    longitude,
+    images,
   } = formData;
-  const handleChange = () => {};
+
+  const handleChange = (e) => {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    // Files
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files, // Fixed typo here
+      }));
+    }
+    // Text / Number / Boolean
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if (+discountedPrice >= +regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less then regullar price.");
+      return;
+    }
+    if (images.length > 7) {
+      setLoading(false);
+      toast.error("Maximum 6 iamges are allowed.");
+      return;
+    }
+    let geolocation = {};
+    if (geoLocationEnabled) {
+      const encodedAddress = encodeURIComponent(address);
+
+      const url = `https://map-geocoding.p.rapidapi.com/json?address=${encodedAddress}`;
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": apiKey,
+          "X-RapidAPI-Host": "map-geocoding.p.rapidapi.com",
+        },
+      };
+
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+
+      const location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.geometry.location;
+      if (!location) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
+      geolocation.lat = location.lat ?? 0;
+      geolocation.lng = location.lng ?? 0;
+    } else {
+      geolocation.lat = latitude;
+      geolocation.lng = longitude;
+    }
+
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const filename = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+        const storageRef = ref(storage, filename);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            // Handle unsuccessful uploads
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch((error) => {
+      setLoading(false);
+      toast.error("Image not Uploaded/.");
+    });
+
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
+
+    delete formDataCopy.images;
+    delete formDataCopy.latitude;
+    delete formDataCopy.longitude;
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+    setLoading(false);
+    toast.success("Listing added.");
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <main className="max-w-md px-2 mx-auto">
       <h1 className="text-3xl text-center mt-6 font-bold">Create Listing</h1>
-      <form className="mr-3">
+      <form onSubmit={handleSubmit} className="mr-3">
         <p className="text-xl mt-6 font-semibold">Sell / Rent</p>
         <div className="flex ">
           <button
             type="button"
             id="type"
             value="sale"
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`mr-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               type === "rent"
                 ? "bg-white text-black"
@@ -52,7 +216,7 @@ export default function CreateListing() {
             type="button"
             id="type"
             value="rent"
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`ml-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               type === "sale"
                 ? "bg-white text-black"
@@ -80,7 +244,6 @@ export default function CreateListing() {
             <p className="text-xl font-semibold">Beds</p>
             <input
               type="number"
-              name="bedrooms"
               id="bedrooms"
               value={bedrooms}
               onChange={handleChange}
@@ -94,7 +257,6 @@ export default function CreateListing() {
             <p className="text-xl font-semibold">Baths</p>
             <input
               type="number"
-              name="bathrooms"
               id="bathrooms"
               value={bathrooms}
               onChange={handleChange}
@@ -111,7 +273,7 @@ export default function CreateListing() {
             type="button"
             id="parking"
             value={true}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`mr-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               !parking ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -122,7 +284,7 @@ export default function CreateListing() {
             type="button"
             id="parking"
             value={false}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`ml-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               parking ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -136,7 +298,7 @@ export default function CreateListing() {
             type="button"
             id="furnished"
             value={true}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`mr-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               !furnished ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -147,7 +309,7 @@ export default function CreateListing() {
             type="button"
             id="furnished"
             value={false}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`ml-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               furnished ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -158,7 +320,6 @@ export default function CreateListing() {
         <p className="text-xl mt-6 font-semibold">Address</p>
         <textarea
           type="text"
-          name="address"
           id="address"
           value={address}
           onChange={handleChange}
@@ -166,12 +327,41 @@ export default function CreateListing() {
           minLength={10}
           placeholder="Address"
           required
-          className={`resize-none w-full px-4 py-2 text-xl text-gray-700 border border-gray-300 bg-white rounded transition duration-200 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6`}
+          className={`resize-none w-full px-4 py-2 text-xl text-gray-700 border border-gray-300 bg-white rounded transition duration-200 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 `}
         />
+        {!geoLocationEnabled && (
+          <div className="flex space-x-6 mb-6">
+            <div>
+              <p className="text-xl mt-6 font-semibold">Latitude</p>
+              <input
+                type="number"
+                id="latitude"
+                value={latitude}
+                onChange={handleChange}
+                required
+                min={-90}
+                max={90}
+                className="w-full px-4 py-2 text-center text-xl text-gray-700 border border-gray-300 bg-white rounded transition duration-200 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600  "
+              />
+            </div>
+            <div>
+              <p className="text-xl mt-6 font-semibold">Longitude</p>
+              <input
+                type="number"
+                id="longitude"
+                value={longitude}
+                onChange={handleChange}
+                required
+                min={-180}
+                max={180}
+                className="w-full px-4 py-2 text-center text-xl text-gray-700 border border-gray-300 bg-white rounded transition duration-200 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600  "
+              />
+            </div>
+          </div>
+        )}
         <p className="text-xl  font-semibold">Description</p>
         <textarea
           type="text"
-          name="description"
           id="description"
           value={description}
           onChange={handleChange}
@@ -187,7 +377,7 @@ export default function CreateListing() {
             type="button"
             id="offer"
             value={true}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`mr-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               !offer ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -198,7 +388,7 @@ export default function CreateListing() {
             type="button"
             id="offer"
             value={false}
-            onChange={handleChange}
+            onClick={handleChange} // Changed from onChange to onClick
             className={`ml-3 px-7 py-3 text-sm uppercase font-medium shadow-md rounded hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-200 ease-in-out w-full ${
               offer ? "bg-white text-black" : "text-white bg-slate-600"
             }`}
@@ -261,7 +451,7 @@ export default function CreateListing() {
           </p>
           <input
             type="file"
-            id="iamges"
+            id="images" // Corrected typo from "iamges" to "images"
             onChange={handleChange}
             accept=".png,.jpg,.jpeg"
             multiple
