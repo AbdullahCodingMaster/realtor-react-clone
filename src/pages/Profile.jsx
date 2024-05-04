bcd73e2ea09eb1473ae69fc6c5db9af1e095d8db
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-05-04 10:30:57 +0500

    complete listingItem component.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -149,10 +149,10 @@ const Profile = () => {
           <p>Loading...</p>
         ) : listings.length > 0 ? (
           <>
-            <h2 className="text-2xl text-center mt-6 font-semibold">
+            <h2 className="text-2xl text-center mt-6 font-semibold mb-6">
               My Listings
             </h2>
-            <ul>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
